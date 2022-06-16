8283dfca0329945d4a08c2f79f4daae2734fdceb
Nguyen Dang <63406002+huudang2840@users.noreply.github.com>
2022-06-16 16:20:32 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://huudang284:01685134737dang@cluster0.yxl4h.mongodb.net/?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://huudang2842:!23456@cluster0.aaxpe.mongodb.net/?retryWrites=true&w=majority, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
